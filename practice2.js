5731197ed1ead98c3eef1ab32ba4019ce094de60
Abu Talha <abutalha14199@gmail.com>
2022-08-09 12:45:27 +0600

    Amendment

--- practice2.js
+++ practice2.js
@@ -1,20 +1,31 @@
 const numbers = [124, 235, 354, 576, 677, 878, 889]
 
-function oddNumbers(numbers) {
+function oddNumber(numbers) {
     const requirement = [];
-  const requirement2 =[];
     for (let i = 0; i < numbers.length; i++) {
         const number = numbers[i]
 
         if (number % 2 === 0) {
             requirement.push(number);
         }
-        else {
-            requirement2.push(number);
-            console.log(requirement2);
-        }
     }
     return requirement;
 }
 
-console.log(oddNumbers(numbers));
+
+function evennumber(numbers) {
+    const requirement2 = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i];
+        if (number %2 !== 0) {
+            requirement2.push(number);
+        }
+    }
+    return requirement2;
+}
+
+const oddNumbers = oddNumber(numbers);
+const evennumbers = evennumber(numbers);
+
+console.log('Array Of Odd Number:', oddNumbers);
+console.log('Array Of Even Number:', evennumbers);
